e7da1c0bd3e64fb35d71517efeac919401c33260
Gaurav Shukla <gaurav.shukla@singtel.com>
2019-12-31 16:40:01 +0800

    Final Demo

--- script.js
+++ script.js
@@ -9,122 +9,98 @@ function getEndPoints(){
         return res.json();
     })
     .then(function(data){
-        console.log(data);
        maxWidth = data.limit;
         var i;
-        var btnCount = data.buttons.length;
 
-      if(data.IsMaxBarBtn){
+        //check whether max number of buttons is ristricted
+        var btnCount = data.buttons.length;
+        if(data.IsMaxBarBtn){
         if(btnCount>6){
             btnCount=6;
         } else btnCount;
-     }
-
+        }
+        
+        //create dynamic buttons
         for(i=0;i<btnCount;i++){
             var btn = document.createElement("button");
            btn.id = "btn"+[i];
            btn.className = "actionBtn";
            btn.innerHTML= data.buttons[i];
-           console.log(data.buttons[i]);
-           //btn.addEventListener('click', function(){btnAction(this.innerHTML)} )
            btn.onclick = function(){btnAction(this.innerHTML);}
            document.getElementById('allButtonsContainer').appendChild(btn);
-           //document.getElementsByClassName('actionBtn')[i].innerHTML= data.buttons[i];
         }
-
+         
+        //check whether max number of bars is ristricted
         var barCount = data.bars.length;
-        if(data.IsMaxBarBtn){ if(barCount>5){
+        if(data.IsMaxBarBtn){ 
+            if(barCount>5){
             barCount=5;
         } else barCount;}
-
-        for(i=0;i<barCount;i++){
-            document.getElementById('allBarsContainer').innerHTML += `<div id="progressContainer"><span class="value"></span><div class="progress">
-            </div></div>`
-            document.getElementsByClassName('progress')[i].style.setProperty("width",data.bars[i]+"%");
-            document.getElementsByClassName('value')[i].innerHTML= Math.abs(data.bars[i])+"%";
-
-            setColor(Math.abs(data.bars[i]),i);
-
-            var select = document.getElementById('dropdown');
-            var seq = i;
-            seq++;
-            var option = document.createElement("option"),txt = document.createTextNode("#Progress"+seq);
-            option.appendChild(txt);
-            option.setAttribute("value", seq);
-            select.insertBefore(option,select.lastChild);
-        }
         
+        //create dynamic bars
+        for(i=0;i<barCount;i++){
+        document.getElementById('allBarsContainer').innerHTML += `<div id="progressContainer"><span class="value"></span><div class="progress">
+        </div></div>`
+        setWidth(data.bars[i],i);
+        setColor(data.bars[i],i);
+
+        //crete dynamic dropdown items
+        var select = document.getElementById('dropdown');
+        var seq = i;
+        seq++;
+        var option = document.createElement("option"),txt = document.createTextNode("#Progress"+seq);
+        option.appendChild(txt);
+        option.setAttribute("value", seq);
+        select.insertBefore(option,select.lastChild);
+        }    
     });
 }
 
+//on button click action
 function btnAction(btn){
-    actionValue = parseFloat(btn);
-   //var maxWidth=160;
-   console.log(actionValue);
+   actionValue = parseFloat(btn);
    var pbCurrIndex= getSelectedValue();
    var oldWidth = parseFloat(getComputedStyle( document.getElementsByClassName('progress')[pbCurrIndex]).getPropertyValue('width'));
    var container = parseFloat(getComputedStyle(document.getElementById('progressContainer')).getPropertyValue('width'));
    var newWidth = Math.round(((oldWidth * 100 )/container) + (actionValue));
-   
-   if(newWidth<=0){
-       newWidth = 0;
-   } else if(newWidth>=maxWidth){
-       newWidth=maxWidth;
-   }else newWidth;
-
-   document.getElementsByClassName('progress')[pbCurrIndex].style.setProperty("width",newWidth+"%");
-   document.getElementsByClassName('value')[pbCurrIndex].innerHTML= newWidth + "%";
-
+   setWidth(newWidth,pbCurrIndex);
    setColor(newWidth,pbCurrIndex);
 }
 
+// To get select dropdown value
 function getSelectedValue(){
     var pbCurrIndex=0;
     var dd = document.getElementById('dropdown').value;
     pbCurrIndex= dd-1;
-
-    /*switch(dd){
-        case 'p1' :
-            pbCurrIndex=0;
-            break;
-         case 'p2' :
-             pbCurrIndex=1;
-             break; 
-         case 'p3' :
-               pbCurrIndex=2;
-               break; 
-         case 'p4' :
-               pbCurrIndex=3;
-                break;  
-    }*/
     return pbCurrIndex;
  } 
 
+ //To get color for progress bar
  function setColor(width, currIndex){
+     var color;
     if(width>100){
-        document.getElementsByClassName('progress')[currIndex].style.setProperty("background-color","#f39c12")
+       color= "#f39c12"
        }else if(width<=100){
-        document.getElementsByClassName('progress')[currIndex].style.setProperty("background-color","#2980b9") 
+        color ="#2980b9" 
+       } else if (width=0){
+        color = "#f5f5f5"
        }
+       document.getElementsByClassName('progress')[currIndex].style.setProperty("background-color",color)
  }
- 
- /*function addBars(a){
-     //var dynamicaBar = document.createElement('div');
-     for(i=0;i<a;i++){
-     document.getElementById('allBarsContainer').innerHTML += `<div id="progressContainer"><div class="progress">
-     <span class="value"></span></div>`
-     }
-    }*/
 
-    /*function addButtons(a){
-        for(i=0; i<a; i++){
-           var btn = document.createElement("button");
-           btn.id = "btn"+[i];
-           btn.class = "actionBtn";
-           btn.onclick= "btnAction(" + "btn"+[i] +")";
-          document.getElementById('allButtonsContainer').appendChild(btn);
-            //<button id="btn1" class="actionBtn" onclick="btnAction('btn1')"></button>
-        }
-    }*/
+ // To get innet HTML and width of progress bar
+ function setWidth(width,currIndex){
+     if(width<0){
+         width = 0;
+     }else if (width>maxWidth){
+         width=maxWidth;
+     }else width;
+
+     document.getElementsByClassName('progress')[currIndex].style.setProperty("width",width +"%");
+     document.getElementsByClassName('value')[currIndex].innerHTML= width + "%";
+     }
+     
+ 
+
      
      
